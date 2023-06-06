2b23316b2c67b0c6d05090ec8067ec5325959cbd
QuintyHR <70649864+QuintyHR@users.noreply.github.com>
2023-06-06 10:13:04 +0200

    Added the stylesheet for styling the app

--- ChairApp/App.js
+++ ChairApp/App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useStyling } from './styles/style.js';
 
 export default function App() {
+  const styling = useStyling;
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styling.header}>Welkom!</Text>
+      <Text style={[styling.paragraph, styling.center]}>Vul je de 4-cijferige code in die te vinden is aan de binnenkant van de stoel.</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ ChairApp/styles/style.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+export const useStyling = StyleSheet.create({
+    header: {
+        fontSize: 40,
+        fontFamily: 'Klara',
+    },
+    
+    paragraph: {
+        fontSize: 16,
+        fontFamily: 'Quicksand',
+    },
+    
+    center: {
+        textAlign: 'center',
+    }
+});
