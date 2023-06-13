46256644bc4a82af4d6f1e05aabd65e18bf04852
QuintyHR <70649864+QuintyHR@users.noreply.github.com>
2023-06-13 09:45:02 +0200

    Added all the styling for the scrollview items

--- ChairApp/components/Themes.js
+++ ChairApp/components/Themes.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { StyleSheet, Text, Image } from 'react-native';
+import { StyleSheet, Text, Image, View } from 'react-native';
 import { Button, Flex, Center, ScrollView } from "native-base";
 import { useStyling } from '../styles/style.js';
 import { LinearGradient } from 'expo-linear-gradient';
@@ -14,98 +14,124 @@ export const Themes = ({navigation}) => {
         colors={['#EAC7B3', '#FBF4F0']}
         style={[styles.container, styles.background]}
       >
-        <Text style={[styling.h1, styling.colorBlack, { fontFamily: 'Cookie-Regular' }]}>Naar Home</Text>
-        <Text style={[styling.h1, styling.colorBlack, { fontFamily: 'Cookie-Regular' }]}>Thema's</Text>
-        <Button
-          title="Go to Welcome"
-          onPress={() => navigation.navigate('Home')}
-        >Go back</Button>
+        <Text style={[styling.paragraph, styling.colorBlack, { fontFamily: 'Quicksand-500', justifyContent: "flex-start" }]}>Naar Home</Text>
+        <View style={styles.center}>
+            <Text style={[styling.h1, styling.colorBlack, { fontFamily: 'Cookie-Regular' }]}>Thema's</Text>
+            <Button
+            title="Go to Welcome"
+            onPress={() => navigation.navigate('Home')}
+            >Go back</Button>
 
-        <ScrollView>
-            <Flex direction="row" mb="2.5" mt="1.5">
-                <Flex direction="col" mb="2.5" mt="1.5">
-                    <Center width="88" height="97" borderRadius="10" bg="#B2CAC0">
-                        <Image source={require('../assets/images/hemelse_sereniteit.svg')} style={styles.image} />
-                    </Center>
-                    <Text>Hemelse Sereniteit</Text>
+            <ScrollView w="300" h="600">
+                <Flex direction="row" mb="2.5" mt="1.5">
+                    <Flex direction="col" mb="2.5" mt="1.5" w="100">
+                        <Center width="88" height="97" borderRadius="10" bg="#B2CAC0">
+                            <Image source={require('../assets/images/hemelse_sereniteit.svg')} style={styles.image} />
+                        </Center>
+                        <Center style={styling.center} width="88">
+                            <Text style={[styling.paragraphSmaller, styling.colorBlack, { fontFamily: 'Quicksand-500'}]}>Hemelse Sereniteit</Text>
+                        </Center>
+                    </Flex>
+                    <Flex direction="col" mb="2.5" mt="1.5" w="100">
+                        <Center width="88" height="97" borderRadius="10" bg="#B2CAC0">
+                            <Image source={require('../assets/images/energieke_landschappen.svg')} style={styles.image} />
+                        </Center>
+                        <Center style={styling.center} width="88">
+                            <Text style={[styling.paragraphSmaller, styling.colorBlack, { fontFamily: 'Quicksand-500'}]}>Energieke Landschappen</Text>
+                        </Center>
+                    </Flex>
+                    <Flex direction="col" mb="2.5" mt="1.5" w="100">
+                        <Center width="88" height="97" borderRadius="10" bg="#B2CAC0">
+                            <Image source={require('../assets/images/magisch_stadsbos.svg')} style={styles.image} />
+                        </Center>
+                        <Center style={styling.center} width="88">
+                            <Text style={[styling.paragraphSmaller, styling.colorBlack, { fontFamily: 'Quicksand-500'}]}>Magisch Stadsbos</Text>
+                        </Center>
+                    </Flex>
                 </Flex>
-                <Flex direction="col" mb="2.5" mt="1.5">
-                    <Center width="88" height="97" borderRadius="10" bg="#B2CAC0">
-                        <Image source={require('../assets/images/energieke_landschappen.svg')} style={styles.image} />
-                    </Center>
-                    <Text>Energieke Landschappen</Text>
-                </Flex>
-                <Flex direction="col" mb="2.5" mt="1.5">
-                    <Center width="88" height="97" borderRadius="10" bg="#B2CAC0">
-                        <Image source={require('../assets/images/magisch_stadsbos.svg')} style={styles.image} />
-                    </Center>
-                    <Text>Magisch Stadsbos</Text>
-                </Flex>
-            </Flex>
 
-            <Flex direction="row" mb="2.5" mt="1.5">
-                <Flex direction="col" mb="2.5" mt="1.5">
-                    <Center width="88" height="97" borderRadius="10" bg="#B2CAC0">
-                        <Image source={require('../assets/images/revitaliserende_energie.svg')} style={styles.image} />
-                    </Center>
-                    <Text>Revitaliserende Energie</Text>
+                <Flex direction="row" mb="2.5" mt="1.5">
+                    <Flex direction="col" mb="2.5" mt="1.5" w="100">
+                        <Center width="88" height="97" borderRadius="10" bg="#B2CAC0">
+                            <Image source={require('../assets/images/revitaliserende_energie.svg')} style={styles.image} />
+                        </Center>
+                        <Center style={styling.center} width="88">
+                            <Text style={[styling.paragraphSmaller, styling.colorBlack, { fontFamily: 'Quicksand-500'}]}>Revitaliserende Energie</Text>
+                        </Center>
+                    </Flex>
+                    <Flex direction="col" mb="2.5" mt="1.5" w="100">
+                        <Center width="88" height="97" borderRadius="10" bg="#B2CAC0">
+                            <Image source={require('../assets/images/ontdek_de_wereld_nabij.svg')} style={styles.image} />
+                        </Center>
+                        <Center style={styling.center} width="88">
+                            <Text style={[styling.paragraphSmaller, styling.colorBlack, { fontFamily: 'Quicksand-500'}]}>Ontdek de wereld nabij</Text>
+                        </Center>
+                    </Flex>
+                    <Flex direction="col" mb="2.5" mt="1.5" w="100">
+                        <Center width="88" height="97" borderRadius="10" bg="#B2CAC0">
+                            <Image source={require('../assets/images/utopia.svg')} style={styles.image} />
+                        </Center>
+                        <Center style={styling.center} width="88">
+                            <Text style={[styling.paragraphSmaller, styling.colorBlack, { fontFamily: 'Quicksand-500'}]}>Utopia</Text>
+                        </Center>
+                    </Flex>
                 </Flex>
-                <Flex direction="col" mb="2.5" mt="1.5">
-                    <Center width="88" height="97" borderRadius="10" bg="#B2CAC0">
-                        <Image source={require('../assets/images/ontdek_de_wereld_nabij.svg')} style={styles.image} />
-                    </Center>
-                    <Text>Ontdek de wereld nabij</Text>
-                </Flex>
-                <Flex direction="col" mb="2.5" mt="1.5">
-                    <Center width="88" height="97" borderRadius="10" bg="#B2CAC0">
-                        <Image source={require('../assets/images/utopia.svg')} style={styles.image} />
-                    </Center>
-                    <Text>Utopia</Text>
-                </Flex>
-            </Flex>
 
-            <Flex direction="row" mb="2.5" mt="1.5">
-                <Flex direction="col" mb="2.5" mt="1.5">
-                    <Center width="88" height="97" borderRadius="10" bg="#B2CAC0">
-                        <Image source={require('../assets/images/hemelse_sereniteit.svg')} style={styles.image} />
-                    </Center>
-                    <Text>Hemelse Sereniteit</Text>
+                <Flex direction="row" mb="2.5" mt="1.5">
+                    <Flex direction="col" mb="2.5" mt="1.5" w="100">
+                        <Center width="88" height="97" borderRadius="10" bg="#B2CAC0">
+                            <Image source={require('../assets/images/hemelse_sereniteit.svg')} style={styles.image} />
+                        </Center>
+                        <Center style={styling.center} width="88">
+                            <Text style={[styling.paragraphSmaller, styling.colorBlack, { fontFamily: 'Quicksand-500'}]}>Hemelse Sereniteit</Text>
+                        </Center>
+                    </Flex>
+                    <Flex direction="col" mb="2.5" mt="1.5" w="100">
+                        <Center width="88" height="97" borderRadius="10" bg="#B2CAC0">
+                            <Image source={require('../assets/images/energieke_landschappen.svg')} style={styles.image} />
+                        </Center>
+                        <Center style={styling.center} width="88">
+                            <Text style={[styling.paragraphSmaller, styling.colorBlack, { fontFamily: 'Quicksand-500'}]}>Energieke Landschappen</Text>
+                        </Center>
+                    </Flex>
+                    <Flex direction="col" mb="2.5" mt="1.5" w="100">
+                        <Center width="88" height="97" borderRadius="10" bg="#B2CAC0">
+                            <Image source={require('../assets/images/magisch_stadsbos.svg')} style={styles.image} />
+                        </Center>
+                        <Center style={styling.center} width="88">
+                            <Text style={[styling.paragraphSmaller, styling.colorBlack, { fontFamily: 'Quicksand-500'}]}>Magisch Stadsbos</Text>
+                        </Center>
+                    </Flex>
                 </Flex>
-                <Flex direction="col" mb="2.5" mt="1.5">
-                    <Center width="88" height="97" borderRadius="10" bg="#B2CAC0">
-                        <Image source={require('../assets/images/energieke_landschappen.svg')} style={styles.image} />
-                    </Center>
-                    <Text>Energieke Landschappen</Text>
-                </Flex>
-                <Flex direction="col" mb="2.5" mt="1.5">
-                    <Center width="88" height="97" borderRadius="10" bg="#B2CAC0">
-                        <Image source={require('../assets/images/magisch_stadsbos.svg')} style={styles.image} />
-                    </Center>
-                    <Text>Magisch Stadsbos</Text>
-                </Flex>
-            </Flex>
 
-            <Flex direction="row" mb="2.5" mt="1.5">
-                <Flex direction="col" mb="2.5" mt="1.5">
-                    <Center width="88" height="97" borderRadius="10" bg="#B2CAC0">
-                        <Image source={require('../assets/images/revitaliserende_energie.svg')} style={styles.image} />
-                    </Center>
-                    <Text>Revitaliserende Energie</Text>
+                <Flex direction="row" mb="2.5" mt="1.5">
+                    <Flex direction="col" mb="2.5" mt="1.5" w="100">
+                        <Center width="88" height="97" borderRadius="10" bg="#B2CAC0">
+                            <Image source={require('../assets/images/revitaliserende_energie.svg')} style={styles.image} />
+                        </Center>
+                        <Center style={styling.center} width="88">
+                            <Text style={[styling.paragraphSmaller, styling.colorBlack, { fontFamily: 'Quicksand-500'}]}>Revitaliserende Energie</Text>
+                        </Center>
+                    </Flex>
+                    <Flex direction="col" mb="2.5" mt="1.5" w="100">
+                        <Center width="88" height="97" borderRadius="10" bg="#B2CAC0">
+                            <Image source={require('../assets/images/ontdek_de_wereld_nabij.svg')} style={styles.image} />
+                        </Center>
+                        <Center style={styling.center} width="88">
+                            <Text style={[styling.paragraphSmaller, styling.colorBlack, { fontFamily: 'Quicksand-500'}]}>Ontdek de wereld nabij</Text>
+                        </Center>
+                    </Flex>
+                    <Flex direction="col" mb="2.5" mt="1.5" w="100">
+                        <Center width="88" height="97" borderRadius="10" bg="#B2CAC0">
+                            <Image source={require('../assets/images/utopia.svg')} style={styles.image} />
+                        </Center>
+                        <Center style={styling.center} width="88">
+                            <Text style={[styling.paragraphSmaller, styling.colorBlack, { fontFamily: 'Quicksand-500'}]}>Utopia</Text>
+                        </Center>
+                    </Flex>
                 </Flex>
-                <Flex direction="col" mb="2.5" mt="1.5">
-                    <Center width="88" height="97" borderRadius="10" bg="#B2CAC0">
-                        <Image source={require('../assets/images/ontdek_de_wereld_nabij.svg')} style={styles.image} />
-                    </Center>
-                    <Text>Ontdek de wereld nabij</Text>
-                </Flex>
-                <Flex direction="col" mb="2.5" mt="1.5">
-                    <Center width="88" height="97" borderRadius="10" bg="#B2CAC0">
-                        <Image source={require('../assets/images/utopia.svg')} style={styles.image} />
-                    </Center>
-                    <Text>Utopia</Text>
-                </Flex>
-            </Flex>
-        </ScrollView>
+            </ScrollView>
+        </View>
       </LinearGradient>
   );
 }
@@ -114,6 +140,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+  center: {
     alignItems: 'center',
     justifyContent: 'center',
   },
